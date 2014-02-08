ee6e881f092485c72272023fdf31791a6c65294d
pikesley <github@orgraphone.org>
2014-02-08 22:00:35 +0000

    I R Enumerable

--- lib/ultron/characters.rb
+++ lib/ultron/characters.rb
@@ -3,11 +3,21 @@ module Ultron
     def initialize
     end
 
-    def results
-      cnxn = Ultron::Connection.new 'characters'
-      res = cnxn.perform
+#    include Enumerable
 
-      res['data']['results']
+#    def each
+#      @results.each { |result| yield result }
+#    end
+
+    def [] key
+      results[key]
+    end
+
+    def results
+      @results || begin
+        cnxn     = Ultron::Connection.new 'characters'
+        cnxn.perform['data']['results']
+      end
     end
   end
 end
--- spec/ultron/characters_spec.rb
+++ spec/ultron/characters_spec.rb
@@ -3,14 +3,19 @@ module Ultron
   describe Characters do
     before :each do
       @characters = Ultron::Characters.new
+      Timecop.freeze '2014-02-08T21:20:00+00:00'
     end
 
-    it 'should return a list of characters' do
-      @characters.results.class.should == Array
+    after :each do
+      Timecop.return
     end
+
+#    it 'should return a list of characters' do
+#      @characters.results.class.should == Array
+#    end
 
     it 'should have the correct first character', :vcr do
-      @characters.results[0]['id'].should == 1009521
+      @characters[0]['id'].should == 1009521
     end
   end
 end
