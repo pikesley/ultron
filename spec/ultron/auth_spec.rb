ba65a7ea8a470b06b9c10f21d4ab710405aec0a4
pikesley <github@orgraphone.org>
2014-02-08 21:33:44 +0000

    Spec

---
+++ spec/ultron/auth_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+module Ultron
+  describe Ultron do
+    context 'auth' do
+      it 'should return the correct string' do
+        Timecop.freeze '2014-02-08T21:20:00+00:00'
+        Ultron.auth('public', 'private').should == 'ts=1391894400&apikey=private&hash=7b0ecfd5af9875bc436cfd5ecc58ef3d'
+        Timecop.return
+      end
+    end
+  end
+end
