4fa85188f5e4a159b1b02fd089e0c8d56130cc50
pikesley <github@orgraphone.org>
2014-02-08 21:32:51 +0000

    Do some auth magic

--- lib/ultron/helpers.rb
+++ lib/ultron/helpers.rb
@@ -6,4 +6,20 @@ module Ultron
         s
     )
   end
+
+  def self.auth private_key, public_key
+    timestamp = Time.new.strftime '%s'
+
+    digest = Digest::MD5.hexdigest '%s%s%s' % [
+        timestamp,
+        private_key,
+        public_key
+    ]
+
+    'ts=%s&apikey=%s&hash=%s' % [
+        timestamp,
+        public_key,
+        digest
+    ]
+  end
 end
