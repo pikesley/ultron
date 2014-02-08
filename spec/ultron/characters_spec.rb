fc2e4580c4dbe1c965b48f8ed48f83f5ae8afeb9
pikesley <github@orgraphone.org>
2014-02-08 21:33:07 +0000

    Specs

--- spec/ultron/characters_spec.rb
+++ spec/ultron/characters_spec.rb
@@ -8,5 +8,13 @@ module Ultron
     it 'should have a url' do
       @characters.url.to_s.should == 'http://gateway.marvel.com/v1/public/characters'
     end
+
+    it 'should return a list of characters' do
+      @characters.results.class.should == Array
+    end
+
+    it 'should have the correct first character', :vcr do
+      @characters.results[0]['id'].should == 1009521
+    end
   end
 end
--- spec/ultron/helpers_spec.rb
+++ spec/ultron/helpers_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 module Ultron
   describe Ultron do
-    it 'should return the correct url' do
-      Ultron.get_url('doge').to_s.should == 'http://gateway.marvel.com/v1/public/doge'
+    context 'helpers' do
+      it 'should return the correct url' do
+        Ultron.get_url('doge').to_s.should == 'http://gateway.marvel.com/v1/public/doge'
+      end
     end
   end
 end
