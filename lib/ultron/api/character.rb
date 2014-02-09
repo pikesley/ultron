f01eae37f96e8c9956ba43574607cbf109c34b40
pikesley <github@orgraphone.org>
2014-02-09 20:43:02 +0000

    Factor all the things

--- lib/ultron/api/character.rb
+++ lib/ultron/api/character.rb
@@ -10,9 +10,13 @@ module Ultron
         results[key]
       end
 
+      def method_missing method_name, *args
+        results.send(method_name)
+      end
+
       def results
         @results || begin
-          @cnxn.perform['data']['results'].first
+          OpenStruct.new @cnxn.perform['data']['results'].first
         end
       end
 
--- lib/ultron/api/comic.rb
+++ lib/ultron/api/comic.rb
@@ -6,14 +6,6 @@ module Ultron
         @cnxn = Ultron::Connection.new 'comics/%s' % @id
       end
 
- #     def title
- #       Ultron.title_extractor results['title']
- #     end
-
- #     def year
- #       Ultron.year_extractor results['title']
- #     end
-
       def [] key
         results[key]
       end
