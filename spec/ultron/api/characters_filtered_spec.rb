91a5a9bd187ddacfd45dad1ba3a259e78c59f7cc
pikesley <github@orgraphone.org>
2014-02-10 19:51:24 +0000

    Creator filters

--- spec/ultron/api/characters_filtered_spec.rb
+++ spec/ultron/api/characters_filtered_spec.rb
@@ -9,25 +9,25 @@ module Ultron
 
       context 'generate filtered lists of characters' do
         it 'by comic', :vcr do
-          @set = Ultron::API::Characters.new 'comics/29506/characters'
+          @set = Characters.new 'comics/29506/characters'
           @set.first['id'].should == 1009257
           @set.first['name'].should == 'Cyclops'
         end
 
         it 'by event', :vcr do
-          @set = Ultron::API::Characters.new 'events/116/characters'
+          @set = Characters.new 'events/116/characters'
           @set.last['id'].should == 1009282
           @set.last['name'].should == 'Doctor Strange'
         end
 
         it 'by series', :vcr do
-          @set = Ultron::API::Characters.new 'series/150/characters'
+          @set = Characters.new 'series/150/characters'
           @set[1]['id'].should == 1009368
           @set[1]['name'].should == 'Iron Man'
         end
 
         it 'by story', :vcr do
-          @set = Ultron::API::Characters.new 'stories/44081/characters'
+          @set = Characters.new 'stories/44081/characters'
           @set.first['id'].should == 1009726
           @set.first['name'].should == 'X-Men'
         end
--- spec/ultron/api/comics_filtered_spec.rb
+++ spec/ultron/api/comics_filtered_spec.rb
@@ -9,31 +9,31 @@ module Ultron
 
       context 'generate filtered lists of comics' do
         it 'by character', :vcr do
-          @set = Ultron::API::Comics.new 'characters/1009496/comics'
+          @set = Comics.new 'characters/1009496/comics'
           @set.first['id'].should == 41839
           @set.first['title'].should == 'Uncanny X-Force (2010) #13 (2nd Printing Variant)'
         end
 
         it 'by creator', :vcr do
-          @set = Ultron::API::Comics.new 'creators/196/comics'
+          @set = Comics.new 'creators/196/comics'
           @set.last['id'].should == 46246
           @set.last['title'].should == 'Marvel Masterworks: Golden Age All-Winners (Trade Paperback)'
         end
 
         it 'by event', :vcr do
-          @set = Ultron::API::Comics.new 'events/314/comics'
+          @set = Comics.new 'events/314/comics'
           @set.first['id'].should == 48176
           @set.first['title'].should == 'Hunger (2013) #4'
         end
 
         it 'by series', :vcr do
-          @set = Ultron::API::Comics.new 'series/3688/comics'
+          @set = Comics.new 'series/3688/comics'
           @set.last['id'].should == 18828
           @set.last['title'].should == 'Spider-Man & Wolverine (2003) #2'
         end
 
-        it 'by stories', :vcr do
-          @set = Ultron::API::Comics.new 'stories/3228/comics'
+        it 'by story', :vcr do
+          @set = Comics.new 'stories/3228/comics'
           @set.first['id'].should == 5032
           @set.first['title'].should == 'X-Men: The End Book 3: Men and X-Men (Trade Paperback)'
         end
