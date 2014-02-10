9526b4a60277b314ac508e6cf30a08a984df07d1
pikesley <github@orgraphone.org>
2014-02-10 13:28:39 +0000

    Aha
    Still lots of redundancy here I think

--- spec/ultron/api/character_set_spec.rb
+++ spec/ultron/api/character_set_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 module Ultron
   module API
-    describe CharacterSet do
+    describe Characters do
       before :each do
         Timecop.freeze '2014-02-08T21:20:00+00:00'
-        @set = Ultron::API::CharacterSet.new 'comics/29506'
+        @set = Ultron::API::Characters.new 'comics/29506/characters'
       end
 
       after :each do
