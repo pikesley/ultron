7815a1a5a0aabfa4c29e816824b69119deeaa1f2
pikesley <github@orgraphone.org>
2014-02-15 15:16:10 +0000

    Excise cruft

--- lib/ultron/connection.rb
+++ lib/ultron/connection.rb
@@ -2,43 +2,18 @@ Dotenv.load
 
 module Ultron
   class Connection
-    attr_accessor :path
-
-    def add_params h
-      @params ||= {}
-      h.each_pair do |key, value|
-        @params[key] = URI.encode value.to_s
-      end
-    end
-
-    def remove_param p
-      @params ||= {}
-      @params.delete p
-    end
-
-    def url
-      @url || begin
-        url = "%s?%s" % [
-            Ultron.get_url(@path),
-            Ultron.auth(ENV['PRIVATE_KEY'], ENV['PUBLIC_KEY'])
-        ]
-        url = "%s&%s" % [url, ((@params.map { |k, v| "#{k}=#{v}" }).join '&')] if @params
-
-        url
-     end
-    end
-
     def self.perform url
-      c = Curl::Easy.new("%s" % url)
+      c         = Curl::Easy.new("%s" % url)
       c.headers = {
-          'Accept' => 'application/json'
+          'Accept'     => 'application/json',
           'User-agent' => 'Ultron v%s: https://rubygems.org/gems/ultron' % Ultron::VERSION
       }
-      if Config.instance.config['debug']
-        puts '>>> Hitting %s' % url
-      end
-      c.perform
 
+  #    if Config.instance.config['debug']
+  #      puts '>>> Hitting %s' % url
+  #    end
+
+      c.perform
       JSON.parse c.body_str
     end
   end
