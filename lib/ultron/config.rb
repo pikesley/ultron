e82b6afdbcf1377b9bfae0a3ed06f5e2da6eb638
pikesley <github@orgraphone.org>
2014-02-15 15:15:46 +0000

    Neaten up

--- lib/ultron/config.rb
+++ lib/ultron/config.rb
@@ -6,7 +6,7 @@ module Ultron
       reset!
     end
 
-    def reset!
+    def reset! # testing a singleton is hard
       @config = OpenStruct.new fetch_yaml 'ultron'
     end
 
@@ -24,18 +24,7 @@ module Ultron
     private
 
     def fetch_yaml file
-      YAML.load(
-          File.open(
-              File.join(
-                  File.dirname(__FILE__),
-                  '..',
-                  '..',
-                  'config/%s.yaml' % [
-                      file
-                  ]
-              )
-          )
-      )
+      YAML.load(File.open(File.join(File.dirname(__FILE__), '..', '..', 'config/%s.yaml' % file)))
     end
   end
 end
