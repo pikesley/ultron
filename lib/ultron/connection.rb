6dcd9f7c520575bfd5f15c4555147db51e483eb5
pikesley <github@orgraphone.org>
2014-02-15 15:16:01 +0000

    User-agent

--- lib/ultron/connection.rb
+++ lib/ultron/connection.rb
@@ -32,6 +32,7 @@ module Ultron
       c = Curl::Easy.new("%s" % url)
       c.headers = {
           'Accept' => 'application/json'
+          'User-agent' => 'Ultron v%s: https://rubygems.org/gems/ultron' % Ultron::VERSION
       }
       if Config.instance.config['debug']
         puts '>>> Hitting %s' % url
