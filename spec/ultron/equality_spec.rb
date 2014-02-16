5b3f7f4ee85de96a8d1ce810059d62fa122deac8
pikesley <github@orgraphone.org>
2014-02-16 22:05:39 +0000

    Baffling

--- spec/ultron/equality_spec.rb
+++ spec/ultron/equality_spec.rb
@@ -4,7 +4,7 @@ module Ultron
   describe 'equality' do
     it 'should be equal if its URL (minus the auth bit) is equal', :vcr do
 #      pending 'Because this throws Travis into a flat spin'
-      Timecop.freeze '1970-01-01'
+      Timecop.freeze '1974-06-16'
       a = Comics.by_character 1009472 # Nightcrawler
 
       Timecop.freeze '1974-06-15'
