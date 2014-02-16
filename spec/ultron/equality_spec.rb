d17673a708961d78cc292b5729cefc546a9088e0
pikesley <github@orgraphone.org>
2014-02-16 21:17:22 +0000

    WTAF TRAVIS?

--- spec/ultron/equality_spec.rb
+++ spec/ultron/equality_spec.rb
@@ -5,6 +5,8 @@ module Ultron
     it 'should be equal if its URL (minus the auth bit) is equal', :vcr do
       Timecop.freeze '1970-01-01'
       a = Comics.by_character 1009472 # Nightcrawler
+
+      Timecop.freeze '1974-06-15'
       b = Comics.by_character 1009472
 
       a.should == b
