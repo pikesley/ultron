c651be95aac6305ec724354f112f88adc3f27c20
pikesley <github@orgraphone.org>
2014-02-16 22:24:08 +0000

    Oh I see. Timecop.return

--- spec/ultron/equality_spec.rb
+++ spec/ultron/equality_spec.rb
@@ -4,10 +4,10 @@ module Ultron
   describe 'equality' do
     it 'should be equal if its URL (minus the auth bit) is equal', :vcr do
 #      pending 'Because this throws Travis into a flat spin'
-      Timecop.freeze '2014-02-16T00:00:00+00:00'
+      Timecop.freeze '1970-01-01T00:00:00+00:00'
       a = Comics.by_character 1009472 # Nightcrawler
 
-      Timecop.freeze '2014-02-15T00:00:00+00:00'
+      Timecop.freeze '1974-06-15T00:00:00+00:00'
       b = Comics.by_character 1009472
 
       a.should == b
