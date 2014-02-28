deac373439e14b5770f70a784c60e2ec1b17110d
pikesley <github@orgraphone.org>
2014-02-28 10:41:00 +0000

    Proper exception tests

--- spec/ultron/exceptions_spec.rb
+++ spec/ultron/exceptions_spec.rb
@@ -7,45 +7,38 @@ module Ultron
     end
 
     it 'should throw a 404 (wrapped in a Marvel exception) on a 404', :vcr do
-      begin
-        comic = Comics.find 1000000 # there are not a million comics
-      rescue MarvelException => e
-        e.code.should == 404
-        e.status.should == "We couldn't find that comic_issue"
-      end
+      expect { Comics.find 1000000 }.to raise_exception { |exception| # there are not a million comics
+        exception.should be_a MarvelException
+        exception.code.should == 404
+        exception.status.should == "We couldn't find that comic_issue"
+      }
     end
 
     it 'should throw a No Idea What This Param Is exception', :vcr do
-      begin
-        character = Characters.where Thor: 'Mighty'
-      rescue MarvelException => e
-        e.code.should == 409
-        e.status.should == "We don't recognize the parameter Thor"
-      end
+      expect { Characters.where(Thor: 'Mighty') }.to raise_exception { |exception|
+        exception.should be_a MarvelException
+        exception.code.should == 409
+        exception.status.should == "We don't recognize the parameter Thor"
+      }
     end
 
     it 'should throw an Ultron exception when we do something dumb', :vcr do
-      begin
-        comics = Comics.where offset: 1000000
-      rescue UltronException => e
-        e.status.should == 'The search returned no results'
-      end
+      expect { Comics.where offset: 1000000 }.to raise_exception { |exception|
+        exception.status.should == 'The search returned no results'
+      }
     end
 
     it 'should throw a Resource Not Found exception when we search for something nonsensical', :vcr do
-      begin
-        characters = Characters.by_creator 186 # characters by creator is a nonsense concept in the Marvel API
-      rescue UltronException => e
-        e.status.should == 'Resource does not exist. Check http://developer.marvel.com/docs'
-      end
+      expect { Characters.by_creator 186 }.to raise_exception { |exception| # characters by creator is a nonsense concept in the Marvel API
+        exception.status.should == 'Resource does not exist. Check http://developer.marvel.com/docs'
+      }
     end
 
     it 'should throw a Resource Throttled Exception when we hit our limit', :vcr do
-      begin
-        events = Events.get
-      rescue UltronException => e
-        e.status.should == 'You have exceeded your rate limit. Please try again later'
-      end
+      pending 'Guess I need to stub this request'
+      expect { Events.get }.to raise_exception { |exception|
+        exception.status.should == 'You have exceeded your rate limit. Please try again later'
+      }
     end
 
     after :each do
